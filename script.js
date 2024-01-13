984355fd05df7039014428dbaaa71f1cdd8114a0
Ashwin3207 <58184287+Ashwin3207@users.noreply.github.com>
2024-01-13 21:32:02 +0530

    code done

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let num=Math.floor(Math.random()*10);
-console.log(num);
+let num=Math.floor(Math.random()*100);
 
+function play()
+{
+    let gnum=document.getElementById("input").value;
+    if(gnum==num)
+    alert("U guessed correct! ");
+    else if(gnum>num)
+    alert("Enter a smaller number !");
+    else
+    alert("Enter a greater number!");
+
+}
