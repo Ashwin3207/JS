4bf0ff6c5eaacdef2e1b7a25babfbcee7875d7c6
Ashwin3207 <58184287+Ashwin3207@users.noreply.github.com>
2024-01-13 23:07:26 +0530

    changes

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 let num=Math.floor(Math.random()*100);
-
+let lives=5;
 function play()
 {
     let gnum=document.getElementById("input").value;
-    console.log(gnum);
+    
     if(gnum==num)
-    document.getElementById("wtext").innerHTML="Your guess is correct! ";
+    {
+        document.getElementById("wtext").innerHTML="Your guess is correct! ";
+    }
+   
     else if(gnum>num)
-    document.getElementById("wtext").innerHTML="Enter a smaller number! ";
+    {
+        document.getElementById("wtext").innerHTML="Enter a smaller number! ";
+        lives--;
+        document.getElementById("life").innerHTML=lives;
+    }
+    
     else if(gnum=="")
-    document.getElementById("wtext").innerHTML="Enter a number! ";
+    {
+        document.getElementById("wtext").innerHTML="Enter a number! ";
+    }
+    
     else
-    document.getElementById("wtext").innerHTML="Enter a larger number! ";
+    {
+        document.getElementById("wtext").innerHTML="Enter a larger number! ";
+        lives--;
+        document.getElementById("life").innerHTML=lives;
+    }
+    
+
+    
+
+
 
 }
