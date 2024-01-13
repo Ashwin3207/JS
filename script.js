21cf6d29f84cd9a6fd98c1143afc339852f19b04
Ashwin3207 <58184287+Ashwin3207@users.noreply.github.com>
2024-01-13 23:11:41 +0530

    sec added

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let num=Math.floor(Math.random()*100);
+let cxe12=Math.floor(Math.random()*100);
 let lives=5;
 function play()
 {
     let gnum=document.getElementById("input").value;
     
-    if(gnum==num)
+    if(gnum==cxe12)
     {
         document.getElementById("wtext").innerHTML="Your guess is correct! ";
     }
    
-    else if(gnum>num)
+    else if(gnum>cxe12s)
     {
         document.getElementById("wtext").innerHTML="Enter a smaller number! ";
         lives--;
